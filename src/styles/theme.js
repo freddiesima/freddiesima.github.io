96194561f0aabbc5f1ca2453e76b513259b68c6f
Freddie_S <76829445+freddiesima@users.noreply.github.com>
2021-01-02 20:21:45 +0900

    Initialize Material-UI

---
+++ src/styles/theme.js
@@ -0,0 +1,18 @@
+import { createMuiTheme} from "@material-ui/core"
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#6f74dd',
+      main: '#3949ab',
+      dark: '#00227b',
+      contrastText: '#ffffff'
+    },
+    secondary: {
+      light: '#63a4ff',
+      main: '#1976d2',
+      dark: '#004ba0',
+      contrastText: '#ffffff'
+    }
+  }
+})
